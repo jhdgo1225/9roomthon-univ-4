e963e69ff8adcf47712c1131ef054ade6baf4930
jhdgo1225 <parkjh99365@gmail.com>
2025-05-13 00:43:41 +0900

    REFACTOR: change Normal CSS into vanilla-extract

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+// next.config.ts
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withVanillaExtract = createVanillaExtractPlugin();
 
-export default nextConfig;
+const nextConfig: NextConfig = {};
+
+export default withVanillaExtract(nextConfig);
